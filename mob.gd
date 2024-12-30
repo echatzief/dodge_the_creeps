ea71cc8c6f7eb625df71e4bbfb1624bcd9313084
echatzief <echatzief97@gmail.com>
2024-12-30 22:07:27 +0200

    Generate random scaled mobs

--- mob.gd
+++ mob.gd
@@ -4,6 +4,11 @@ func _ready():
 	$AnimatedSprite2D.play()
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
+	
+	# Pick a random scale mob
+	var random_mob_scale = randf_range(0.5, 1) 
+	$AnimatedSprite2D.scale.x = random_mob_scale
+	$AnimatedSprite2D.scale.y = random_mob_scale
 
 
 func _on_VisibilityNotifier2D_screen_exited():
